424d6e788a2ba4f808795d0839dfe974594526fc
Kush Thaker <thakerkush12@gmail.com>
2023-06-06 15:27:19 +0000

    midway fixing ping

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,10 @@ import fs from "fs";
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
   ],
 });
 declare module "discord.js" {
@@ -19,39 +21,91 @@ declare module "discord.js" {
   }
 }
 
-client.config = {
-  prefix: "/",
-  playing: "In development",
-};
-
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-async function loadCommands() {
-  client.commands = new Collection();
-
-  const commandFiles = fs.readdirSync(`${__dirname}/commands`);
-
-  for (const file of commandFiles) {
-    try {
-      console.log("does this run first?");
-      const command: any = await import(`${__dirname}/commands/${file}`); //Change now
-      client.commands.set(command.name, command);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
-loadCommands();
-client.on("message", function (messages) {
-  if (messages.content.toLocaleLowerCase() === "hello")
-    messages.channel.send("hello" + " " + messages.author.username); //reply hello word message with senders name
-});
-
 const color = {
   white: "\x1B[0m",
   grey: "\x1B[2m",
   green: "\x1B[32m",
   FgRed: "\x1b[31m",
 };
-client.login(DC_TOKEN);
+
+client.config = {
+  prefix: "/",
+  playing: "In development",
+};
+
+client.commands = new Collection();
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, (interaction) => {
+  
+  console.log("INTERACTATION HIT!");
+  console.log(interaction.isChatInputCommand());
+
+  if (!interaction.isChatInputCommand()) return;
+  console.log(interaction);
+});
+
+
+const loadEvents = () => {
+  console.log(`-> loading Events ......`);
+  return new Promise<void>(async (resolve, reject) => {
+    const events = fs.readdirSync(`${__dirname}/events/`);
+
+    console.log(`+--------------------------------+`);
+    for (const file of events) {
+      try {
+        const event = await import(`${__dirname}/events/${file}`);
+
+        // Take everything before the dot,
+        // So if the filename is `testFile.ts`, the variable will be `testFile`
+        const eventName = file.split(".")[0];
+
+        client.on(eventName, event.default.bind(null, client));
+
+        //padEnd just adds 17 chars of empty space so it looks good
+        console.log(`| Loaded event ${eventName.padEnd(17, " ")} |`);
+      } catch (error) {
+        reject(error);
+      }
+    }
+    console.log(`+--------------------------------+`);
+    console.log(`${color.grey}-- loading Events finished --${color.white}`);
+
+    resolve();
+  });
+};
+
+const loadCommands = () => {
+  console.log(`-> loading Commands ......`);
+  return new Promise<void>(async (resolve, reject) => {
+    const jsFiles = fs.readdirSync(`${__dirname}/commands/`);
+
+    console.log(`+---------------------------+`);
+    for (const file of jsFiles) {
+      try {
+        const command = await import(`${__dirname}/commands/${file}`);
+        client.commands.set(command.name.toLowerCase(), command);
+        console.log(
+          `| Loaded Command ${command.name.toLowerCase().padEnd(10, " ")} |`
+        );
+      } catch (error) {
+        reject(error);
+      }
+    }
+    console.log(`+---------------------------+`);
+    console.log(`${color.grey}-- loading Commands finished --${color.white}`);
+
+    resolve();
+  });
+};
+
+Promise.resolve()
+  .then(() => loadEvents())
+  .then(() => loadCommands())
+  .then(() => {
+    console.log(`${color.green}*** All loaded successfully ***${color.white}`);
+    client.login(DC_TOKEN);
+  });
--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,7 +1,18 @@
+import { Client, CommandInteraction } from "discord.js";
+
 export const name = "ping";
-export const description = "Replies With Pong!";
-export const execute = async (interaction) => {
-  await interaction.reply(
+export const aliases = ["p"];
+export const showHelp = false;
+export const description = "Ping command";
+export const options = [];
+
+export const slashExecute = async (
+  client: Client,
+  interaction: CommandInteraction
+) => {
+  console.log("HEREEEE!");
+
+  return interaction.reply(
     `🏓Latency is ${Date.now() - interaction.createdTimestamp}ms`
   );
 };
